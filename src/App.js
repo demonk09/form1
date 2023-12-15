51a67b3e8893b99e3cfaf52ec42b027b84cb3511
Mayur Kamble <54568942+demonk09@users.noreply.github.com>
2023-12-15 11:50:20 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App() {
+  let a=["HTML","CSS","JS","REACT","ANGULER"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <h1>FrontEnd Tech...</h1>
+      {
+        a.map((item,index)=><li key={index}>{item}</li>)
+      }
     </div>
-  );
+  )
 }
-
-export default App;
